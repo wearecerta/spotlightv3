c0754ed50dabed6a65ffa7d3558a6b6f81bd348a
Mikiyas Begashaw <mickeyjob2004@gmail.com>
2026-01-02 09:17:56 +0300

    add tumbnail image on the home page hero while the video loading

--- components/sections/Hero.tsx
+++ components/sections/Hero.tsx
@@ -16,6 +16,7 @@ export default function Hero({ videoAsset, title, subtitle }: HeroProps) {
           className="absolute top-0 left-0 w-full h-full object-cover"
           src={videoUrl}
           autoPlay
+          poster="/Home/hero-templete.jpg"
           muted
           loop
           playsInline
