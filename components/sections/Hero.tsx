1677802ea2523baf02f5827f20ff9859d5bc0040
Mikiyas Begashaw <mickeyjob2004@gmail.com>
2026-01-22 14:17:04 +0300

    optimize hero section

--- components/sections/Hero.tsx
+++ components/sections/Hero.tsx
@@ -1,5 +1,5 @@
-'use client';
 import { getVideoUrl } from "@/sanity/lib/videoUrl";
+
 interface HeroProps {
   videoAsset?: any;
   title?: string;
@@ -11,38 +11,36 @@ export default function Hero({ videoAsset, title, subtitle }: HeroProps) {
 
   return (
     <section className="relative w-full h-screen -mt-20 md:-mt-26 lg:-mt-26 overflow-hidden z-0 bg-(--spotlight-950)">
+      {!videoUrl && (
+        <img
+          src="/Home/tumbnail.webp"
+          alt="Hero background"
+          fetchPriority="high"
+          className="absolute top-0 left-0 w-full h-full object-cover"
+        />
+      )}
+
       {videoUrl && (
         <video
           className="absolute top-0 left-0 w-full h-full object-cover"
           src={videoUrl}
-          autoPlay
           poster="/Home/tumbnail.webp"
+          preload="auto"
+          autoPlay
           muted
           loop
           playsInline
-          onEnded={(e) => {
-            const v = e.currentTarget as HTMLVideoElement;
-            v.currentTime = 0;
-            v.play();
-          }}
         />
       )}
-
-      {!videoUrl && (
-        <div className="absolute inset-0 bg-gray-900"></div>
-      )}
-
-      {/* Overlay for better text readability */}
-      {/* <div className="absolute inset-0 bg-black/40"></div> */}
 
       {/* Centered content */}
       <div className="relative z-10 flex flex-col items-center justify-center h-full text-center px-4">
         <h1
-          className="text-white font-medium  leading-(--h1-line) text-[5.75rem] md:text-[12rem]"
-          style={{ fontFamily: "var(--font-primary)", 
+          className="text-white font-medium leading-(--h1-line) text-[5.75rem] md:text-[12rem]"
+          style={{
+            fontFamily: "var(--font-primary)",
             mixBlendMode: "overlay",
-            opacity: 0.4
-            
+            opacity: 0.4,
           }}
         >
           SHINE BRIGHT
